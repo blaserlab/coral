cc4bc9b64b5d294906b19ab7318e93071b699971
blaserlab <bradley.blaser@osumc.edu>
2024-11-23 14:20:25 -0500

    changed file paths and conda env

--- coral/run_os_benchmarking.sh
+++ coral/run_os_benchmarking.sh
@@ -1,19 +1,19 @@
 # qsub -q "gpu.q" -pe smp 1 -l gpu_mem=40G -l h_rt=336:00:00 -cwd -j yes -o logs/os_benchmarking.log run_os_benchmarking.sh
 
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate base
-module load cuda
-export PYTHONPATH=~/Papers/Onc-PN/coral/:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=$SGE_GPU
+source /workspace/python/anaconda3/etc/profile.d/conda.sh
+conda activate coral
+# module load cuda
+export PYTHONPATH=../:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES=1
 
-echo "GPU allocation: ${SGE_GPU}"
+echo "GPU allocation: $CUDA_VISIBLE_DEVICES"
 
-gemma_model="/wynton/protected/project/shared_models/gemma/gemma-7b-it/"
-annot_data_dir='../data/annotated/'
-fdata='data_onc_pn_ie.csv'
-fout='output_onc_pn_ie.csv'
-dir_data='../data/'
-dir_out='../output/'
+gemma_model="/workspace/refdata/LLM_curation_data/models/models--google--gemma-7b-it/snapshots/9c5798d27f588501ce1e108079d2a19e4c3a2353/"
+annot_data_dir='/workspace/refdata/LLM_curation_data/coral_data/annotated/'
+fdata='breastca_unannotated.csv'
+fout='breastca_out.csv'
+dir_data='/workspace/refdata/LLM_curation_data/coral_data/unannotated/data'
+dir_out='../coral_output/'
 
 echo "Creating inference data"
 python -u dataprocessor/create_inference_data.py \
